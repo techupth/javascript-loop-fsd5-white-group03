4f6cac8ec8261347316c3b2fd5b27f77f3cafddd
hedisnice <nicenice60225@gmail.com>
2023-11-29 12:10:01 +0700

    feat(exercise) : add complete exercise

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,9 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
-
+for (let i = 0; i < numbers.length; i++) {
+  if (minNumber === undefined || numbers[i] < minNumber) {
+    minNumber = numbers[i];
+  }
+}
 console.log(minNumber);
