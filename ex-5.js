9f84e6b1ee39da1cc3dca081b825b453aa0b51f6
pednaruemol <naruemol.kphet@gmail.com>
2023-12-01 15:36:51 +0700

    finished:exercise1-6 Loop

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,23 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
 
-// Start coding here
+// // Start coding here
+// ----------1**** number not run
+// let letter;
+// let i;
+//    for (letter of companyName) {
+//     console.log(letter)
+//     for ( i = 0; i < letter.length; i++) {
+//      console.log(`Number ${i+1} character is ${letter[i]}`);
+//     }
+//   }
+
+//----------2
+for (let i = 0; i < companyName.length; i++) {
+  console.log("Number " + (i + 1) + " character is " + companyName[i]);
+}
+
+//----------3
+// for(let i=0; i<companyName.length;i++){
+//     console.log(`Number ${i+1} character is ${companyName[i]}`)
+// }
